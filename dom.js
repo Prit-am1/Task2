e8f389fb5de00315f8c34976c2f5bd308723ecc6
Prit-am1 <roychowdhurypritam372@gmail.com>
2023-06-21 00:24:16 +0530

    new li added and modified

--- dom.js
+++ dom.js
@@ -4,3 +4,8 @@ for(let ele of modify){
 }
 let color = document.getElementsByClassName("list-group-item");
 color[2].style.backgroundColor = "green";
+
+let li = document.getElementsByTagName("li");
+li[4].style.backgroundColor = "yellow";
+let tag = document.getElementsByClassName("new-li");
+tag[0].innerHTML = "<h4>Item 5</h4>"
