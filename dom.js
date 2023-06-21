47bc8393d5c4b9eecedec5bb5356d01889444163
Prit-am1 <roychowdhurypritam372@gmail.com>
2023-06-21 17:17:47 +0530

    modified using querySelectorAll

--- dom.js
+++ dom.js
@@ -1,11 +1,20 @@
-let modify = document.getElementsByClassName("list-group");
-for(let ele of modify){
-    ele.style.fontWeight = "bold";
-}
-let color = document.getElementsByClassName("list-group-item");
-color[2].style.backgroundColor = "green";
+// let modify = document.getElementsByClassName("list-group");
+// for(let ele of modify){
+//     ele.style.fontWeight = "bold";
+// }
+// let color = document.getElementsByClassName("list-group-item");
+// color[2].style.backgroundColor = "green";
 
-let li = document.getElementsByTagName("li");
-li[4].style.backgroundColor = "yellow";
-let tag = document.getElementsByClassName("new-li");
-tag[0].innerHTML = "<h4>Item 5</h4>"
+// let li = document.getElementsByTagName("li");
+// li[4].style.backgroundColor = "yellow";
+// let tag = document.getElementsByClassName("new-li");
+// tag[0].innerHTML = "<h4>Item 5</h4>"
+
+let ele1 = document.querySelectorAll(".list-group-item");
+ele1[1].style.backgroundColor = "green";
+ele1[1].style.color = "green";
+ele1[2].style.visibility = "hidden";
+let ele2 = document.querySelectorAll("li:nth-child(odd)");
+for(let odd of ele2){
+    odd.style.backgroundColor = "green";
+}
